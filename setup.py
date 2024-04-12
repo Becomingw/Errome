1e0114ab77559973bcaccd9a01d0b4f058791ace
Becomingw <1173586205@qq.com>
2024-04-12 20:11:29 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fp:
+    long_description = fp.read()
 setup(
     name='Errome',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
-    description='一个程序报错或运行完成时的邮件通知工具',
-    long_description='未来补充，目前仅支持网易邮箱',
+    description='A program error or completion email notification tool.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='BecomingW',
     author_email='Becomingw@qq.com',
     url='https://github.com/Becomingw/Errome.git',
